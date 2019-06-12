72a89fb08512a06c0eac9a270ae39bb59c98d3f1
Ian Carlos <carlos.ian007@gmail.com>
2019-06-11 22:34:14 -0300

    Feat(structure): Add project structure and server

---
+++ api/README.md
@@ -0,0 +1,17 @@
+# Instagram Clone - API
+
+The backend side of the project
+
+## Available Scripts
+
+In the project directory, as in api as in interface, install the depedencies.
+
+```bash
+yarn install
+```
+
+Run, as in api as in interface directory, run the projects;
+
+```bash
+yarn dev
+```
---
+++ api/src/server.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+
+const databaseConfig = require('./config/database')
+
+class App {
+  constructor () {
+    this.express = express()
+
+    this.database()
+    this.middlewares()
+    this.routes()
+  }
+
+  database () {
+    mongoose.connect(databaseConfig.uri, {
+      useNewUrlParser: true,
+      useFindAndModify: true
+    })
+  }
+
+  middlewares () {
+    this.express.use(express.json())
+  }
+
+  routes () {
+    this.express.use(require('./routes'))
+  }
+}
+
+module.exports = new App().express
